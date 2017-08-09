ea621569460f4d20c2795a4f457f320649d0a188
CheapSeth <cheapseth@electric-dragons.org>
2017-08-09 22:15:41 +0200

    Added Views, registered them to URLs

--- gamelibrary/urls.py
+++ gamelibrary/urls.py
@@ -1,10 +1,25 @@
 from django.conf.urls import url
 
-from gamelibrary.views import GameDetailView
+from gamelibrary.views import (GameDetailView,
+                               DevelopperRelatedListView,
+                               EditorRelatedListView,
+                               GenreRelatedListView)
 
 
 urlpatterns = \
               [
+                  url(r'^editor/(?P<slug>[-\w]+)/$',
+                      EditorRelatedListView.as_view(),
+                      name="game-list-editor"),
+
+                  url(r'^developper/(?P<slug>[-\w]+)/$',
+                      DevelopperRelatedListView.as_view(),
+                      name="game-list-dev"),
+
+                  url(r'^genre/(?P<slug>[-\w]+)/$',
+                      GenreRelatedListView.as_view(),
+                      name="game-list-genre"),
+
                   url(r'^(?P<slug>[-\w]+)/$',
                       GameDetailView.as_view(),
                       name="game-detail"),
--- gamelibrary/views.py
+++ gamelibrary/views.py
@@ -1,8 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
-from gamelibrary.models import Game
+from gamelibrary.models import (Game,
+                                GameDevelopper,
+                                GameEditor,
+                                GameGenre)
 
 
 class GameDetailView(DetailView):
     model = Game
+
+
+class DevelopperRelatedListView(ListView):
+
+    def get_queryset(self):
+        """
+        Fetches Games related to a developper defined in the "slug" url kwarg
+        """
+
+        developper = get_object_or_404(GameDevelopper,
+                                       slug=self.kwargs["slug"])
+
+        return developper.developped_games.all()
+
+
+class EditorRelatedListView(ListView):
+    """
+    View listing all Game objects related to a specific editor
+    """
+
+    def get_queryset(self):
+        """
+        Fetches Games related to an editor defined in the "slug" url kwarg
+        """
+
+        editor = get_object_or_404(GameEditor, slug=self.kwargs["slug"])
+
+        return editor.edited_games.all()
+
+
+class GenreRelatedListView(ListView):
+    """
+    View listing all game objects related to a specific genre
+    """
+
+    def get_queryset(self):
+        """
+        Fetches Games related to a genre defined in the "slug" url kwarg
+        """
+
+        genre = get_object_or_404(GameGenre, slug=self.kwargs["slug"])
+
+        return genre.related_games.all()
